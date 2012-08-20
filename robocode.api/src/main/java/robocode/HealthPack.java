01b72b1daf27e6c4dc55ed28f8ff1f9e0ac9058a
Jared Hayward <jared.hayward@uqconnect.edu.au>
2012-08-20 11:33:44 +1000

    Added a healthpack class which extends itemdrop

---
+++ robocode.api/src/main/java/robocode/HealthPack.java
@@ -0,0 +1,17 @@
+package robocode;
+
+/**
+ * A health pack item. Extends item drop
+ * @author s4238358
+ *
+ */
+
+public class HealthPack extends ItemDrop {
+
+	public HealthPack(){
+		System.out.println("Health Pack");
+	}
+	
+	
+	
+}
