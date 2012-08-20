982c0856dcc347ccf92f03584e8c50e82b19b888
Jared Hayward <jared.hayward@uqconnect.edu.au>
2012-08-20 11:23:20 +1000

    Added abstract class for item drop

---
+++ robocode.api/src/main/java/robocode/ItemDrop.java
@@ -0,0 +1,17 @@
+package robocode;
+
+/**
+ * Abstract class for item/powerup drops
+ * 
+ * @author s4238358
+ *
+ */
+
+
+public class ItemDrop {
+
+	public ItemDrop(){
+		System.out.println("Item made");
+	}
+	
+}
