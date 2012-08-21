5a4578e81db1227504b31c0eaf7cc1819a207226
Tom Conroy <tom@conroy.com.au>
2012-08-21 23:10:12 +1000

    Made SlowMode extend ClassicMode rather than implement IMode

--- robocode.core/src/main/java/net/sf/robocode/mode/SlowMode.java
+++ robocode.core/src/main/java/net/sf/robocode/mode/SlowMode.java
@@ -1,11 +1,9 @@
 package net.sf.robocode.mode;
 
-public class SlowMode implements IMode {
+public class SlowMode extends ClassicMode {
 	public double modifyVelocity(double velocityIncrement) {
 		return velocityIncrement/2;
-		
 	}
-
 	public void execute() {
 		System.out.println("SLow MODE");
 	}
