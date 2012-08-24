c03d28d2604ff9249ad0007dfe83fb8cee9e5a62
JHayward <s4238358@GS122-2158.labs.eait.uq.edu.au>
2012-08-24 12:17:08 +1000

    Started adding basic outline classes to implement the GUI of the items

---
+++ robocode.api/src/main/java/robocode/control/snapshot/IItemSnapshot.java
@@ -0,0 +1,12 @@
+package robocode.control.snapshot;
+
+/**
+ * Interface of an item snapshot at a specific time in a battle.
+ * @author s4238358
+ *
+ */
+
+//TODO implement this class
+public interface IItemSnapshot {
+
+}
