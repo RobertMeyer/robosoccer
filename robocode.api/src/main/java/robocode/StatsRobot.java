a833cec884836fd3135d7d8ba17f114e59648f4f
Malcolm Inglis <me@minglis.id.au>
2012-08-25 22:50:19 +1000

    Fix StatsRobot comments.

--- robocode.api/src/main/java/robocode/StatsRobot.java
+++ robocode.api/src/main/java/robocode/StatsRobot.java
@@ -1,17 +1,17 @@
 package robocode;
 
 /**
- * A type of robot that has stats, that affect the gameplay of the robot. It
- * can equip {@link EquipmentPart}s to affect those stats.
+ * An advanced type of robot that has stats; attributes that affect the gameplay
+ * of the robot. It can equip {@link EquipmentPart}s to affect those stats.
  * 
- * If you have not already, you should create a {@link Robot} first.
+ * If you have not done already, you should create a {@link Robot} or
+ * {@link AdvancedRobot} first.
  * 
  * @author Jayke Anderson
  * @author Malcolm Inglis
  * @author Andrew Schaul
  * @author Jarred Filmer
  */
-public class StatsRobot extends AdvancedRobot {
-	
+public class StatsRobot extends Robot {
 	
 }
