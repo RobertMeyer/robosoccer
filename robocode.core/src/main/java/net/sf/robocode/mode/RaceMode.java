89cfe0a1d08f8d4c6832aaea19c39e39d65efd8c
Daniel Bowden <daniel.bowden@uqconnect.edu.au>
2012-08-27 10:53:23 +1000

    Add race Mode class
    added race mode class that overwrites toString()

---
+++ robocode.core/src/main/java/net/sf/robocode/mode/RaceMode.java
@@ -0,0 +1,9 @@
+package net.sf.robocode.mode;
+
+public class RaceMode extends ClassicMode{
+
+	//overwriting toString() class so our "mode" name is now returned
+	public String toString(){
+		return new String("Race Mode");
+	}
+}
