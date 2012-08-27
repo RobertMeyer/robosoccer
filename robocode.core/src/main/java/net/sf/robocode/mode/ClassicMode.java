9c9df63b91bd670cd5562133d1bf97293738a46b
Enrique Klein <eeklein.g@gmail.com>
2012-08-27 13:21:03 +1000

    Deleted execute() method from IMode, ClassicMode, SlowMode, and Battle

--- robocode.core/src/main/java/net/sf/robocode/mode/ClassicMode.java
+++ robocode.core/src/main/java/net/sf/robocode/mode/ClassicMode.java
@@ -1,16 +1,26 @@
 package net.sf.robocode.mode;
 
+/**
+ * 
+ * Default implementation of the IMode interface. This class models
+ * the default behaviour of a Robocode game.
+ *
+ */
 public class ClassicMode implements IMode {
-	
-	
-	public void execute(){
-		System.out.println("Classic Mode!!!");
-	}
 
+
+	/**
+	 * Modifies the velocity of Robots in a battle
+	 * @param velocityIncrement
+	 * @return a modified Velocity
+	 */
 	public double modifyVelocity(double velocityIncrement) {
 		return velocityIncrement;
-		
 	}
+	
+	/**
+	 * @return Returns the string "Classic Mode"
+	 */
 	
 	public String toString() {
 		return new String("Classic Mode");
