269eee9346068af7c24c89ae91a6d964162de056
JHayward <s4238358@AX104-1982.labs.eait.uq.edu.au>
2012-08-27 11:58:58 +1000

    Added javadoc for itemDrop and more implementation of checkItemCollision

--- robocode.battle/src/main/java/net/sf/robocode/battle/ItemDrop.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/ItemDrop.java
@@ -41,69 +41,131 @@ public abstract	class ItemDrop {
 		this.battleRules = battle.getBattleRules();
 	}
 
+	/**
+	 * Returns the bounding box of the item
+	 * @return a bounding box representing the location of the item
+	 */
 	public BoundingRectangle getBoundingBox(){
 		return boundingBox;
 	}
 
+	/**
+	 * Set the bounding box with the current x & y coordinates
+	 */
 	public void setBoundingBox() {
 		boundingBox.setRect(xLocation - width / 2 + 2, yLocation - height / 2 + 2, width - 4, height - 4);
 	}
 	
+	/**
+	 * Returns the current x-coordinate of the item
+	 * @return a double representing the x-coordinate of the item
+	 */
 	public double getXLocation() {
 		return xLocation;
 	}
-
+	
+	/**
+	 * Sets a new x-coordinate of the item
+	 * @param a double representing the new x-coordinate of the item
+	 */
 	public void setXLocation(double xLocation) {
 		this.xLocation = xLocation;
 	}
 
-	
+	/**
+	 * Returns the current y-coordinate of the item
+	 * @return a double representing the y-coordinate of the item
+	 */
 	public double getYLocation() {
 		return yLocation;
 	}
 
+	/**
+	 * Sets a new y-coordinate of the item
+	 * @param a double representing the new y-coordinate of the item
+	 */
 	public void setYLocation(double yLocation) {
 		this.yLocation = yLocation;
 	}
 
+	/**
+	 * Returns the width of the item
+	 * @return a double representing the width of the item
+	 */
 	public double getWidth(){
 		return width;
 	}
 	
+	/**
+	 * Returns the height of the item
+	 * @return a double representing the height of the item
+	 */
 	public double getHeight(){
 		return height;
 	}
 	
+	/**
+	 * Returns whether the item is destroyable or not.
+	 * This will allow the itemDrop class to expand to things like extra walls, traps, etc
+	 * @return a boolean representing whether the item is destroyable or not
+	 */
 	public boolean getIsDestroyable(){
 		return isDestroyable;
 	}
 	
+	/**
+	 * Sets if the item isDestroyable or not.
+	 * @param a boolean representing whether the item is destroyable or not
+	 */
 	public void setIsDestroyable(boolean isDestroyable){
 		this.isDestroyable = isDestroyable;
 	}
 	
+	/**
+	 * Returns the number of turns the item will be valid for
+	 * @return an integer representing the nummber of turns the item is valid for
+	 */
 	public int getLifespan(){
 		return lifespan;
 	}
 	
+	/**
+	 * Sets the number of turns the item will be valid for
+	 * @param an integer that represents the lifespan of the item
+	 */
 	public void setLifespan(int lifespan){
 		this.lifespan = lifespan;
 	}
 	
+	/**
+	 * Returns the health of the item. If the health is 0 or below, then it will be destroyed when
+	 * a robot collides with the item.
+	 * @return a double representing the health of the item
+	 */
 	public double getHealth(){
 		return health;
 	}
 	
+	/**
+	 * Set the health of an item
+	 * @param a double representing the new health of the item
+	 */
 	public void setHealth(double health){
 		this.health = health;
 	}
-	
+
 	public boolean getIsEquippable(){
 		return isEquippable;
 	}
 	
 	public void setIsEquippable(boolean isEquippable){
 		this.isEquippable = isEquippable;
+	}
+	
+	/**
+	 * The effect the item has on the robot. Must be overridden to have any effect.
+	 */
+	public void doItemEffect(){
 	}
 	
 	public void initialiseRoundItems(List<RobotPeer> robots, List<ItemDrop> items){
@@ -128,9 +190,9 @@ public abstract	class ItemDrop {
 	public boolean initialiseNewItem(List<RobotPeer> robots, List<ItemDrop> items, double x, double y){
 		xLocation = x;
 		yLocation = y;
-		setBoundingBox();
 		if (validSpotRobot(robots)) {
 			if (validSpotItem(items)){
+				setBoundingBox();
 				return true;
 			}
 			else{
