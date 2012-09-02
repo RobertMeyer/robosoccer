10af864c8b2d96396072a1b9c1b1d6f089b018d2
Daniel Bowden <daniel.bowden@uqconnect.edu.au>
2012-09-02 14:49:37 +1000

    comments added
    added comments to mode class + a couple of basic methods

--- robocode.core/src/main/java/net/sf/robocode/mode/ClassicMode.java
+++ robocode.core/src/main/java/net/sf/robocode/mode/ClassicMode.java
@@ -17,3 +17,4 @@ public class ClassicMode implements IMode {
 	}
 	
 }
+ 
--- robocode.core/src/main/java/net/sf/robocode/mode/RaceMode.java
+++ robocode.core/src/main/java/net/sf/robocode/mode/RaceMode.java
@@ -1,12 +1,37 @@
 package net.sf.robocode.mode;
 
+/**
+ * Mode class used to implement Racing Mode functional enhancement. See ticket #32
+ * 
+ * @author Team - GoGoRobotRacer
+ * @author s4203648
+ */
+
 public class RaceMode extends ClassicMode{
 
+	//private class variables
+	private int noLaps;
+	
+	/**
+	 * Called from the GUI to set the number of laps in the race.
+	 * <p>
+	 * @param laps Number of laps in the race. 0 < noLaps.
+	 * 
+	 */
+	public void setLaps(int laps){
+		if(0 <= laps){
+			throw new IllegalArgumentException("Number of laps must be positive");
+		}else{
+			this.noLaps = laps;
+		}
+		return;
+	}
+	
 	public void execute(){
 		System.out.println("Race Mode.");
 	}
 	
-	//overwriting toString() class so our "mode" name is now returned
+	//overwriting toString() method so our "mode" name is now returned
 	public String toString(){
 		return new String("Race Mode");
 	}
