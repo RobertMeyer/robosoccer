9c930c2e0063f4fa634d44059b86e259fa9507f7
unknown <s4265484@GS116-1940.labs.eait.uq.edu.au>
2012-09-02 13:33:02 +1000

    Added id's to ItemDrop and set Flag id to 1

--- robocode.battle/src/main/java/net/sf/robocode/battle/Flag.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/Flag.java
@@ -1,20 +1,26 @@
 package net.sf.robocode.battle;
 
-import java.lang.Math;
+import net.sf.robocode.battle.peer.RobotPeer;
 
 public class Flag extends ItemDrop {
+	
+	// Variable to indicate the carrier of the flag
+	RobotPeer carrier;
 
 	Flag(boolean isDestroyable, int lifespan, double health,
-			boolean isEquippable, Battle battle) {
+			boolean isEquippable, Battle battle, RobotPeer carrier) {
 		super(isDestroyable, lifespan, health, isEquippable, battle);
 		
-		this.setXLocation(Math.random() * 40);
-		this.setYLocation(Math.random() * 40);
 		this.setIsDestroyable(false);
 		this.setLifespan(lifespan);
 		this.setIsEquippable(true);
+		this.carrier = carrier;
+		this.setId(1);
+		
 	}
 	
-	
+	public RobotPeer getCarrier() {
+		return carrier;
+	}
 
 }
