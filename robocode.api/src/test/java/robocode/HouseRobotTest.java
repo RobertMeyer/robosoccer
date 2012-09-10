e6a63b5aa4a492b3d59bbaba32160e94f28650fd
Jack Reichelt <jackreichelt@gmail.com>
2012-09-10 12:04:53 +1000

    Added some initial testing work.
    Fiddled with Mocking. I think I can start on unit tests properly now.

---
+++ robocode.api/src/test/java/robocode/HouseRobotTest.java
@@ -0,0 +1,20 @@
+package robocode;
+
+import java.awt.peer.RobotPeer;
+
+import robocode.HouseRobot;
+import org.junit.*;
+import static org.mockito.Mockito.*;
+
+public class HouseRobotTest {
+
+	@Test
+	public void distanceRemainingTest() {
+		
+		RobotPeer peer = mock(RobotPeer.class);
+		
+		HouseRobot test = new HouseRobot();
+		
+	}
+	
+}
