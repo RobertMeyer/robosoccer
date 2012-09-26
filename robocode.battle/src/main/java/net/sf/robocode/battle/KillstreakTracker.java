c1cb67ffefbb81c246cfb25fc8b74fe0b15f44a0
Cameron Fletcher <cameronfletcher92@gmail.com>
2012-09-26 20:44:49 +1000

    Fixed a crucial error in updateKillstreaks, and added javadoc comments
    Fixed the error where all robots ere

--- robocode.battle/src/main/java/net/sf/robocode/battle/KillstreakTracker.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/KillstreakTracker.java
@@ -5,28 +5,46 @@ import java.util.Map;
 
 import net.sf.robocode.battle.peer.RobotPeer;
 
-
+/**
+ * Tracks the robot's killstreaks if the checkbox is ticked (or if
+ * enableKillstreaks is otherwise called)
+ **/
 public class KillstreakTracker {
 
 	private Map<RobotPeer, Integer> killstreakRobots = new HashMap<RobotPeer, Integer>();
 	private Battle battle;
 	public static boolean enableKillstreaks = false;
 
+	/**
+	 * Constructor for the KillstreakTracker (called when battle is initialized)
+	 * 
+	 * @param b
+	 *            the battle the tracker is tracking
+	 */
 	public KillstreakTracker(Battle b) {
 		battle = b;
 	}
 
+	/**
+	 * Updates the robots killstreaks when one is killed, resets the victim's
+	 * killstreak and increments the killers
+	 * 
+	 * @param killer
+	 *            The robot who got the kill
+	 * @param victim
+	 *            The robot who got killed
+	 */
 	public void updateKillStreak(RobotPeer killer, RobotPeer victim) {
 		int newKillstreak;
 		if (enableKillstreaks == false) {
 			return;
 		}
-		
+
 		if (killstreakRobots.containsKey(killer)) {
 			newKillstreak = killstreakRobots.get(killer);
 			newKillstreak++;
 			killstreakRobots.put(killer, newKillstreak);
-			
+
 		} else {
 			newKillstreak = 1;
 			killstreakRobots.put(killer, 1);
@@ -39,21 +57,33 @@ public class KillstreakTracker {
 		/* Print disabled due to failing tests */
 		killer.println("SYSTEM: Killstreak is now " + newKillstreak);
 		victim.println("SYSTEM: Killstreak reset to 0");
-		callKillstreaks();
+		callKillstreak(killer);
 	}
 
-	private void callKillstreaks() {
-		for ( RobotPeer robot : killstreakRobots.keySet()) {
+	/**
+	 * Calls the killstreak if the killer has one
+	 * 
+	 * @param robot
+	 *            The robot who got the kill
+	 */
+	private void callKillstreak(RobotPeer robot) {
 
-			switch (killstreakRobots.get(robot)) {
-			case 5:
-				new AirStrike(robot, battle);
-				break;
-			}
-
+		/* check the killers new killstreak */
+		switch (killstreakRobots.get(robot)) {
+		case 5:
+			new AirStrike(robot, battle);
+			break;
 		}
 	}
-	
+
+	/**
+	 * Set the enabled flag for the killstreak tracker. Without the flag,
+	 * killstreaks will not be updated or called. Enable with the checkbox
+	 * in-battle
+	 * 
+	 * @param b
+	 *            the boolean to set the flag
+	 */
 	public static void enableKillstreaks(boolean b) {
 		enableKillstreaks = b;
 	}
