2c14b93d9067b039da596c625c29f17a8d5aa4f0
Cameron Fletcher <cameronfletcher92@gmail.com>
2012-09-26 22:48:59 +1000

    Added the RadarJammer class.
    Sets the isScannable flag in RobotPeer, and it will unset after
    JAM_TIME amount of turns. Robot cannot be scanned for the duration of
    JAM_TIME

---
+++ robocode.battle/src/main/java/net/sf/robocode/battle/RadarJammer.java
@@ -0,0 +1,27 @@
+package net.sf.robocode.battle;
+
+import net.sf.robocode.battle.Battle;
+import net.sf.robocode.battle.peer.BulletPeer;
+import net.sf.robocode.battle.peer.RobotPeer;
+
+/**
+ * Callable ability (used by killstreak tracker. Instantiate one with the robot
+ * who calls the airstrike, and the battle it is in to call the airstrike.
+ * Spawns a series of bullets that don't effect the caller
+ **/
+public class RadarJammer {
+	private final int JAM_TIME = 300;
+	
+	/**
+	 * Constructor for the airstrike being called
+	 * 
+	 * @param robotPeer
+	 *            The robot calling the airstrike
+	 * @param battle
+	 *            The battle that robot is part of
+	 */
+	public RadarJammer(RobotPeer robotPeer, Battle battle) {
+		robotPeer.enableRadarJammer(JAM_TIME);
+		robotPeer.println("SYSTEM: Called a RadarJammer!");
+	}
+}
