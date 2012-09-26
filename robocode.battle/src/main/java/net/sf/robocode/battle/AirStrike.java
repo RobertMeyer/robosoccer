a580f8b3c7a3c79ef415aec4b844023efa5dc1ae
Cameron Fletcher <cameronfletcher92@gmail.com>
2012-09-26 20:49:08 +1000

    Added Javadoc comments to AirStrike

--- robocode.battle/src/main/java/net/sf/robocode/battle/AirStrike.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/AirStrike.java
@@ -4,8 +4,20 @@ import net.sf.robocode.battle.Battle;
 import net.sf.robocode.battle.peer.BulletPeer;
 import net.sf.robocode.battle.peer.RobotPeer;
 
+/**
+ * Callable ability (used by killstreak tracker. Instantiate one with the robot
+ * who calls the airstrike, and the battle it is in to call the airstrike.
+ * Spawns a series of bullets that don't effect the caller
+ **/
 public class AirStrike {
-
+	/**
+	 * Constructor for the airstrike being called
+	 * 
+	 * @param robotPeer
+	 *            The robot calling the airstrike
+	 * @param battle
+	 *            The battle that robot is part of
+	 */
 	public AirStrike(RobotPeer robotPeer, Battle battle) {
 		int battleHeight = battle.getBattleRules().getBattlefieldHeight();
 		for (int i = 0; i < battleHeight; i += 15) {
