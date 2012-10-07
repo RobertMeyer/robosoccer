903659a0dadfa4650d765462788b310cc281544e
Jayke Anderson <jaykeando@gmail.com>
2012-10-07 12:02:38 +1000

    Base for tests for the Equipment Feature

---
+++ robocode.samples/src/main/java/equipment/SampleEquipRobot.java
@@ -0,0 +1,52 @@
+package equipment;
+
+import robocode.*;
+
+/**
+ * A sample equipment robot.  That equips a range of equipment and moves
+ * in a simple circle path.
+ * 
+ * @author Jayke Anderson - CSSE2003
+ */
+public class SampleEquipRobot extends AdvancedRobot {
+	
+	/**
+	 * The main run function
+	 */
+	public void run(){
+		// Equip some items
+		equip("Division 9 Plasmaprojector");
+		equip("Guardian Tank Armor");
+		equip("Thorium Power Cell");
+		equip("Stealth Tracks");
+		
+		// Move in a circle
+		while (true){
+			setAhead(150);
+			setTurnRight(180);
+		}
+	}
+	
+	/**
+	 * Hit a wall: reverse
+	 */
+	public void onHitWall(HitWallEvent e){
+		setBack(200);
+	}
+	
+	/**
+	 * Seen a robot: kill it!
+	 */
+	public void onScannedRobot(ScannedRobotEvent e){
+		fire(3);
+	}
+	
+	/**
+	 * Hit a robot, if at fault, run away!
+	 */
+	public void onHitRobot(HitRobotEvent e){
+		if(e.isMyFault()){
+			setBack(200);
+		}
+	}
+}
---
+++ robocode.tests/src/test/java/net/sf/robocode/test/equipment/TestEquipRobotInPlay.java
@@ -0,0 +1,23 @@
+package net.sf.robocode.test.equipment;
+
+import org.junit.Test;
+import net.sf.robocode.test.helpers.RobocodeTestBed;
+
+/**
+ * Add a basic Equipment Robot to the game.  If the test fails, it is because
+ * the robot has not been added to the game.
+ * 
+ * @author Jayke Anderson
+ *
+ */
+public class TestEquipRobotInPlay extends RobocodeTestBed {
+	
+	/**
+	 * Add an Equipment Robot to the game.
+	 */
+	@Override
+	public String getRobotNames() {
+		return "equipment.SampleEquipRobot";
+	}
+	
+}
