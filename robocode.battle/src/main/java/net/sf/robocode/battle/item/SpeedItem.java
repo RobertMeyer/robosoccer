cc7ce3a268be7acb56075c38383fd6f45d81fb7f
Ghassan Thabit <gthabit@gmail.com>
2012-10-08 19:06:46 +1000

    HaltPack and PoisonPack implemented in item package.
    	- HaltPack halts the robot for the rest of the round.
    	- PoisonPack is supposed to decrease the energy for a certain amount. Not functioning properly yet.

--- robocode.battle/src/main/java/net/sf/robocode/battle/item/SpeedItem.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/item/SpeedItem.java
@@ -1,5 +1,8 @@
 package net.sf.robocode.battle.item;
 
+import net.sf.robocode.battle.Battle;
+import net.sf.robocode.battle.peer.RobotPeer;
+
 
 /**
  * A speed item. Extends item drop
@@ -8,13 +11,13 @@ package net.sf.robocode.battle.item;
  */
 public class SpeedItem extends ItemDrop {
 
-    public SpeedItem(boolean isDestroyable, int lifespan, int health, boolean isEquippable) {
-        super(isDestroyable, lifespan, health, isEquippable, null);
+    public SpeedItem(Battle battle, String name) {
+    	super(true, 400, 0, false, battle);
    //     System.out.println("Speed Item");
         this.name = "SpeedItem";
     }
 
-    public void doItemEffect() {
+    public void doItemEffect(RobotPeer robot) {
         return;
     }
 }
