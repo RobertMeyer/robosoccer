5c47579cb55adbfc81e9d935f9248bad07034c70
huang-eric <eric_9_dragon_phoenix_z@hotmail.com>
2012-10-08 08:37:27 +0800

    background music checkbox

---
+++ robocode.ui/src/main/java/net/sf/robocode/ui/dialog/BackgroundMusicCheckbox.java
@@ -0,0 +1,27 @@
+package net.sf.robocode.ui.dialog;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+
+import net.sf.robocode.battle.BattleProperties;
+import net.sf.robocode.sound.BackgroundMusicTracker;
+
+public class BackgroundMusicCheckbox extends JCheckBox{
+	private static final long serialVersionUID = 1L;
+	public BattleProperties bp;
+	
+	public BackgroundMusicCheckbox() {
+        this.setText("Background Music");
+        ActionListener actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+              BackgroundMusicCheckbox mbox = (BackgroundMusicCheckbox) actionEvent.getSource();
+              BackgroundMusicTracker.enableMusic(mbox.getModel().isSelected());
+              BackgroundMusicTracker.updateStatus();
+            }
+          };
+        this.addActionListener(actionListener);   
+    }
+
+}
