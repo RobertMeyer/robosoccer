RobertMeyer/robosoccer

commit 5c47579cb55adbfc81e9d935f9248bad07034c70

background music checkbox

robocode.sound/src/main/java/net/sf/robocode/sound/BackgroundMusicTracker.java
@@ -1,0 +1,45 @@
+package net.sf.robocode.sound;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class BackgroundMusicTracker {
+	
+	public static boolean music = false;
+	public static Clip clip;
+	public static BackgroundMusicTracker object = new BackgroundMusicTracker();
+	
+	public static void enableMusic(boolean b) {
+		 music = b;
+	}
+	
+	
+	public static void updateStatus(){
+		
+		 if(music){
+			 try{ 
+				AudioInputStream audioIn = AudioSystem.getAudioInputStream(new BackgroundMusicTracker().getClass().getClassLoader().getResourceAsStream("net/sf/robocode/sound/sounds/theme1.wav"));
+         		clip = AudioSystem.getClip(); 
+         		clip.open(audioIn); 
+         		clip.start();
+                clip.loop(-1);
+             }catch(Exception ex){ 
+                 System.out.println("Error with playing sound."); 
+                 ex.printStackTrace(); 
+             }
+		 }else{
+			 try{
+				 clip.stop();
+			 }catch(Exception ex){
+				 System.out.println("Error with playing sound."); 
+                 ex.printStackTrace(); 
+			 }
+		 }
+	}
+	
+
+}

robocode.ui/src/main/java/net/sf/robocode/ui/dialog/BackgroundMusicCheckbox.java
@@ -1,0 +1,27 @@
+package net.sf.robocode.ui.dialog;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+
+import net.sf.robocode.battle.BattleProperties;
+import net.sf.robocode.sound.BackgroundMusicTracker;
+
+public class BackgroundMusicCheckbox extends JCheckBox{
+	private static final long serialVersionUID = 1L;
+	public BattleProperties bp;
+	
+	public BackgroundMusicCheckbox() {
+        this.setText("Background Music");
+        ActionListener actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+              BackgroundMusicCheckbox mbox = (BackgroundMusicCheckbox) actionEvent.getSource();
+              BackgroundMusicTracker.enableMusic(mbox.getModel().isSelected());
+              BackgroundMusicTracker.updateStatus();
+            }
+          };
+        this.addActionListener(actionListener);   
+    }
+
+}

robocode.core/robocode.core.xml
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<module MavenProjectsManager.isMavenModule="true" org.jetbrains.idea.maven.project.MavenProjectsManager.isMavenModule="true" relativePaths="true" type="JAVA_MODULE" version="4">
+  <component name="NewModuleRootManager" LANGUAGE_LEVEL="JDK_1_5" inherit-compiler-output="false">
+    <output url="file://$MODULE_DIR$/target/classes" />
+    <output-test url="file://$MODULE_DIR$/target/test-classes" />
+    <content url="file://$MODULE_DIR$">
+      <sourceFolder url="file://$MODULE_DIR$/src/main/resources" isTestSource="false" />
+      <sourceFolder url="file://$MODULE_DIR$/src/main/java" isTestSource="false" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/java" isTestSource="true" />
+      <excludeFolder url="file://$MODULE_DIR$/target" />
+    </content>
+    <orderEntry type="inheritedJdk" />
+    <orderEntry type="sourceFolder" forTests="false" />
+    <orderEntry type="module" module-name="robocode.api" />
+    <orderEntry type="library" name="Maven: org.picocontainer:picocontainer:2.6" level="project" />
+    <orderEntry type="library" scope="TEST" name="Maven: junit:junit:4.5" level="project" />
+  </component>
+</module>
+

commit 814f196f3198f3e098199c2f57e2b5e64792f7e8

FIxing build of master after commit 5c47579
Commit 5c47579 broke the build of master due to bad coding of the
BackgroundMusicTracker and BackgroundMusicCheckbox. This disables the
problematic code, and will need to be reviewed thoroughly before it is
merged into master again.

robocode.sound/src/main/java/net/sf/robocode/sound/BackgroundMusicTracker.java
@@ -11,7 +11,7 @@ public class BackgroundMusicTracker {
 	
 	public static boolean music = false;
 	public static Clip clip;
-	public static BackgroundMusicTracker object = new BackgroundMusicTracker();
+	//public static BackgroundMusicTracker object = new BackgroundMusicTracker();
 	
 	public static void enableMusic(boolean b) {
 		 music = b;

robocode.ui/src/main/java/net/sf/robocode/ui/dialog/BackgroundMusicCheckbox.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JCheckBox;
 
 import net.sf.robocode.battle.BattleProperties;
-import net.sf.robocode.sound.BackgroundMusicTracker;
+//import net.sf.robocode.sound.BackgroundMusicTracker;
 
 public class BackgroundMusicCheckbox extends JCheckBox{
 	private static final long serialVersionUID = 1L;
@@ -17,8 +17,8 @@ public class BackgroundMusicCheckbox extends JCheckBox{
         ActionListener actionListener = new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
               BackgroundMusicCheckbox mbox = (BackgroundMusicCheckbox) actionEvent.getSource();
-              BackgroundMusicTracker.enableMusic(mbox.getModel().isSelected());
-              BackgroundMusicTracker.updateStatus();
+              //BackgroundMusicTracker.enableMusic(mbox.getModel().isSelected());
+              //BackgroundMusicTracker.updateStatus();
             }
           };
         this.addActionListener(actionListener);   