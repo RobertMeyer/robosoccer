33f2007593cd350ecb4b765c572c2b1b0a2241f2
YE Zhiyuan <tommy_yzy@yahoo.com>
2012-10-08 22:27:00 +1000

    Add the Ricochet test into the robocode.battle
    just create the test, haven't started to write the testing code. In
    addition, added the BulletPeer test in the project, to check out the
    changes can run successfully.

---
+++ robocode.battle/src/test/java/net/sf/robocode/battle/peer/BulletPeerTest.java
@@ -0,0 +1,14 @@
+package net.sf.robocode.battle.peer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BulletPeerTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
