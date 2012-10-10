63279eb9f4712706e943a0b5b72c8476ae9f3660
Lee <leesdolphin@gmail.com>
2012-10-10 20:03:50 +1000

    Merge n
    where n is an increasingly frustrating number

--- robocode.tests/src/main/java/net/sf/robocode/test/helpers/RobocodeTestBed.java
+++ robocode.tests/src/main/java/net/sf/robocode/test/helpers/RobocodeTestBed.java
@@ -120,7 +120,7 @@ public abstract class RobocodeTestBed extends BattleAdaptor {
             final Random random = Utils.getRandom();
 
             if (event.getRobotsCount() == 2) {
-                Assert.assertNear(0.98484154, random.nextDouble());
+                Assert.assertNear(0.24053641567148587, random.nextDouble());
             }
         }
     }
