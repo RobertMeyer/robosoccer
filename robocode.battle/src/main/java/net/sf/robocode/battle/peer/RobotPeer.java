9c4b7c127758ce0b080f307971dd072af7fd96db
Cameron Fletcher <cameronfletcher92@gmail.com>
2012-10-17 00:23:44 +1000

    minor refactoring of animation code

--- robocode.battle/src/main/java/net/sf/robocode/battle/peer/RobotPeer.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/peer/RobotPeer.java
@@ -2741,11 +2741,7 @@ public class RobotPeer implements IRobotPeerBattle, IRobotPeer {
 	 * @return the frozen (killstreak) status
 	 */
 	public boolean isKsFrozen() {
-		if (this.isAlive()) {
-			return this.isKsFrozen;
-		}
-		
-		return false;
+		return this.isKsFrozen;
 	}
 
 	/**
