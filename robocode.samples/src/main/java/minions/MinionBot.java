eee68c22fa35335dbea3fd915e4a2c35c500a3ac
Jordan Henderson <jordan.henderson@uqconnect.edu.au>
2012-10-18 01:54:38 +1000

    Added minion testing bots

---
+++ robocode.samples/src/main/java/minions/MinionBot.java
@@ -0,0 +1,16 @@
+package minions;
+
+import robocode.*;
+
+public class MinionBot extends Minion {
+
+	public void run() {
+		//MinionProxy parent = getParent();
+		
+	}
+
+	@Override
+	public int getMinionType() {
+		return MINION_TYPE_ATK;
+	}
+}
