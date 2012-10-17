dbaf6ba1d3cd87af86f093ca344362567448f38e
Michael Tsai <michael.tsai@uqconnect.edu.au>
2012-10-17 20:57:19 +1000

    Initial attempt to write Obstacle mode unit tests
    It's completely commented out because nothing works yet. Committing this
    so I can pull new stuff.

---
+++ robocode.battle/src/test/java/net/sf/robocode/mode/ObstacleModeTest.java
@@ -0,0 +1,94 @@
+package net.sf.robocode.mode;
+
+import net.sf.robocode.battle.Battle;
+import net.sf.robocode.battle.BattleManager;
+import net.sf.robocode.battle.BattleProperties;
+import net.sf.robocode.battle.events.BattleEventDispatcher;
+import net.sf.robocode.host.CpuManager;
+import net.sf.robocode.host.HostManager;
+import net.sf.robocode.host.security.ThreadManager;
+import net.sf.robocode.recording.RecordManager;
+import net.sf.robocode.repository.RepositoryManager;
+import net.sf.robocode.settings.SettingsManager;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.mockito.Mockito;
+
+import robocode.control.BattleSpecification;
+import robocode.control.BattlefieldSpecification;
+import robocode.control.RobocodeEngine;
+import robocode.control.RobotSpecification;
+
+import static org.junit.Assert.*;
+
+public class ObstacleModeTest {
+	/*BattleProperties bp;
+	BattleManager bm;
+	HostManager hm;
+	SettingsManager sm;
+	RepositoryManager rm;
+	CpuManager cm;
+	BattleEventDispatcher bd;
+	ThreadManager tm;
+	RecordManager rem;
+	Battle battle;
+	
+	RobocodeEngine engine;
+	
+	@Before
+	public void setup() {
+		tm = new ThreadManager();
+		sm = new SettingsManager();
+		hm = new HostManager(sm, tm);
+		rm = new RepositoryManager(sm);
+		cm = new CpuManager(sm);
+		bd = new BattleEventDispatcher();
+		rem = new RecordManager(sm);
+		bm = new BattleManager(sm, rm, hm, cm, bd, rem);
+		
+		bp = Mockito.mock(BattleProperties.class);
+		Mockito.when(bp.getBattlefieldWidth()).thenReturn(800);
+		Mockito.when(bp.getBattlefieldHeight()).thenReturn(600);
+		Mockito.when(bp.getGunCoolingRate()).thenReturn(0.1);
+		Mockito.when(bp.getInactivityTime()).thenReturn((long) 450);
+		Mockito.when(bp.getNumRounds()).thenReturn(1);
+		Mockito.when(bp.getHideEnemyNames()).thenReturn(false);
+		Mockito.when(bp.getSelectedRobots()).thenReturn("sample.Walls, sample.Target");
+		Mockito.when(bp.getInitialPositions()).thenReturn(null);
+		Mockito.when(bp.getBattleMode()).thenReturn(new ObstacleMode());
+
+		//battle = new Battle(sm, bm, hm, rm, cm, bd);
+		//battle.setup(rm.getSelectedRobots(bp.getSelectedRobots()), bp, false, rm);
+		//assert things;
+		RobotSpecification[] rs = engine.getLocalRepository("tested.robots.Ahead, tested.robots.Ahead");
+		
+        assertNotNull("Robot were not loaded", rs);
+        assertEquals("Robot were not loaded", 2, rs.length);
+        engine.runBattle(new BattleSpecification(1, new BattlefieldSpecification(), rs), null, true);
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals("toString method is incorrect", battle.getBattleMode().toString(), "Obstacle Mode");
+	}
+	
+	@Test
+	public void testNumObstacles() {
+		//assertEquals("There are too many obstacles on the battlefield", om.setNumObstacles(null),
+		//		10);
+	}
+	
+	@Test
+	public void testObstacles() {
+		BattleProperties bp = Mockito.mock(BattleProperties.class);
+		// more mocking...
+		 
+		Mockito.when(bp.getBattlefieldWidth()).thenReturn(200);
+		Mockito.when(bp.getBattlefieldHeight()).thenReturn(200);
+		// etc...
+		 
+		//List<ObstaclePeer> obstacles = ObstaclePeer.generateRandomObstacles(10, bp, ...);
+		//assert things;
+	}*/
+}
