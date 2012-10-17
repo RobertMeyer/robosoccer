75697fa10f7880f1b08f5cb371f08096172860cf
liujopu <jopu.liu@uqconnect.edu.au>
2012-10-18 04:13:54 +1000

    MinionList On UI

--- robocode.ui/src/main/java/net/sf/robocode/ui/dialog/NewMinionsModeTab.java
+++ robocode.ui/src/main/java/net/sf/robocode/ui/dialog/NewMinionsModeTab.java
@@ -9,6 +9,8 @@ import java.awt.Label;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -22,6 +24,8 @@ import javax.swing.border.Border;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import net.sf.robocode.repository.IRepositoryItem;
+import net.sf.robocode.repository.IRepositoryManager;
 import net.sf.robocode.ui.IWindowManager;
 import net.sf.robocode.battle.MinionData;
 import robocode._RobotBase;
@@ -38,11 +42,18 @@ public class NewMinionsModeTab extends JPanel{
 	private JTextField minionUtlTxt = new JTextField();
 	private JButton dlgOkBtn = new JButton();
 	private JButton dlgCancelBtn = new JButton();
+	private boolean onlyShowSource;
+	private boolean onlyShowWithPackage;
+	private boolean onlyShowRobots;
+	private boolean onlyShowDevelopment;
+	private boolean onlyShowInJar;
+	private boolean ignoreTeamRobots;
 	private IWindowManager window;
 	private CustomDialog selectMinion;
 	private int currentSelecting = -1;
 	private int currentIndex = -1;
 	private String currentSelection = "";
+<<<<<<< HEAD
 	private class changeHandler implements ChangeListener {
 		@Override
 		public void stateChanged(ChangeEvent ev) {
@@ -50,6 +61,40 @@ public class NewMinionsModeTab extends JPanel{
 			MinionData.setMinionsEnabled(enableMinion.isSelected());
 		}
 	}
+=======
+	public static Object[] minionRobots = null;
+	private RobotSelectionPanel robotSelectionPanel;
+	private IRepositoryManager repository;
+	private List<IRepositoryItem> minionsRepoList = new CopyOnWriteArrayList<IRepositoryItem>();
+	private List<ItemWrapper> minionsList = new CopyOnWriteArrayList<ItemWrapper>();
+	
+	
+	
+	public void minionRobots(int minionType) {
+		
+		Object[] s1 = null;
+		
+		minionsRepoList = new CopyOnWriteArrayList<IRepositoryItem>();
+		minionsList = new CopyOnWriteArrayList<ItemWrapper>();
+		
+		repository = net.sf.robocode.core.Container.createComponent(IRepositoryManager.class);
+		
+		List<IRepositoryItem> minionsRepoList = repository.filterRepositoryItems(onlyShowSource,
+		         onlyShowWithPackage, onlyShowRobots,
+		         onlyShowDevelopment, false,
+		         ignoreTeamRobots, onlyShowInJar, true, minionType);
+		
+		
+		
+		for (IRepositoryItem robotSpec : minionsRepoList) {
+            minionsList.add(new ItemWrapper(robotSpec));
+        }
+
+		minionRobots = minionsList.toArray();
+		
+	}
+	
+>>>>>>> MinionMode List
 	private class buttonHandler implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent ev) {
@@ -60,16 +105,19 @@ public class NewMinionsModeTab extends JPanel{
 			if(caller == selectMinionAtk) {
 				//Create attack selection popup
 				currentSelecting = _RobotBase.MINION_TYPE_ATK;
+				minionRobots(currentSelecting);
 				selectMinion = new CustomDialog(window.getRobocodeFrame(),"Select minions",true,"ATTACK");
 			}
 			else if(caller == selectMinionDef) {
 				//Create defence selection popup
 				currentSelecting = _RobotBase.MINION_TYPE_DEF;
+				minionRobots(currentSelecting);
 				selectMinion = new CustomDialog(window.getRobocodeFrame(),"Select minions",true,"DEFENCE");
 			}
 			else if(caller == selectMinionUtl) {
 				//Create utility selection popup.
 				currentSelecting = _RobotBase.MINION_TYPE_UTL;
+				minionRobots(currentSelecting);
 				selectMinion = new CustomDialog(window.getRobocodeFrame(),"Select minions",true,"UTILITY");
 			}
 			currentIndex = selectMinion.minionList.getSelectedIndex();
@@ -107,9 +155,6 @@ public class NewMinionsModeTab extends JPanel{
 			super(parent,title,modal);
 			int width = 300, height = 300;
 
-			
-			String[] minionRobots = { type , type , type , type , type};
-			
 			minionList.setVisibleRowCount(5);
 			minionList.setAlignmentX(LEFT_ALIGNMENT);
 			minionList.setListData(minionRobots);
@@ -138,6 +183,10 @@ public class NewMinionsModeTab extends JPanel{
 		}
 	}
 	
+	public void minionList(){
+		
+
+	}
 	
 	public NewMinionsModeTab(IWindowManager window) {
 		super();
@@ -146,6 +195,7 @@ public class NewMinionsModeTab extends JPanel{
 	}
 	
 	public void runTab(){
+		
 		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		setLayout(new BorderLayout());
 		add(mainPanel());
@@ -214,5 +264,25 @@ public class NewMinionsModeTab extends JPanel{
 		minionSelect.add(field);
 		minionSelect.add(btn);
 		return minionSelect;
-	}	
+	}
+	
+	public static class ItemWrapper {
+
+        private final IRepositoryItem item;
+
+        public ItemWrapper(IRepositoryItem item) {
+            this.item = item;
+        }
+
+        public IRepositoryItem getItem() {
+            return item;
+        }
+
+        // Used writing the robot name in JList. Is used for keyboard typing in JList to find robot. Bugfix for [2658090]
+        @Override
+        public String toString() {
+            return item.getUniqueShortClassNameWithVersion();
+        }
+    }
+
 }
