44d9bb21e0d598bc0285b3374739a3cba26e1110
Jack Reichelt <jackreichelt@gmail.com>
2012-10-21 07:39:20 +1000

    Commented out MyFirstHouseRobot moving forwards

--- robocode.samples/src/main/java/sampleex/MyFirstHouseRobot.java
+++ robocode.samples/src/main/java/sampleex/MyFirstHouseRobot.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package sampleex;
 
 import java.awt.Color;
@@ -300,3 +301,65 @@ public class MyFirstHouseRobot extends HouseRobot {
 		
 	}
 }
+=======
+package sampleex;
+
+import java.awt.Color;
+
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HouseRobot;
+import robocode.HouseRobotBoundary;
+import robocode.Rules;
+import robocode.ScannedRobotEvent;
+import robocode.TurnCompleteCondition;
+
+public class MyFirstHouseRobot extends HouseRobot {
+	
+	HouseRobotBoundary home;
+	
+	public void run() {
+		
+		setColors(Color.RED, Color.RED, Color.RED);
+		
+		home = getBoundaries();
+		
+		home.setxHome(getX());
+		home.setyHome(getY());
+		home.setInitialFacing(getHeading());
+		home.setArcRange(200);
+		
+		this.turnLeft(80);
+		
+		while (true) {
+			if (this.getScannedRobotEvents().size() == 0) {
+				setTurnRight(160);
+				waitFor(new TurnCompleteCondition(this));
+				setTurnLeft(160);
+				waitFor(new TurnCompleteCondition(this));
+			}
+		}
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e) {		
+		
+		if (e.getDistance() < 200) {
+			
+			setFire(Rules.MAX_BULLET_POWER);
+			turnRight(e.getBearing());
+			//setAhead(10);
+		}
+	}
+	
+	public void onHitRobot(HitRobotEvent e) {		
+			
+		turnRight(e.getBearing());
+		setFire(Rules.MAX_BULLET_POWER);
+	}
+	
+	public void onHitByBullet(HitByBulletEvent e) {
+		turnRight(e.getBearing());
+		setFire(Rules.MAX_BULLET_POWER);
+	}
+}
+>>>>>>> Commented out MyFirstHouseRobot moving forwards
