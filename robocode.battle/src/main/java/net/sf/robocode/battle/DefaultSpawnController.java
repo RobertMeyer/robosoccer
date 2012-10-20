572416bd5568e6935fe9f981177cf04388bbd1c7
Lee <leesdolphin@gmail.com>
2012-10-20 17:32:33 +1000

    Fixe Controller

--- robocode.battle/src/main/java/net/sf/robocode/battle/DefaultSpawnController.java
+++ robocode.battle/src/main/java/net/sf/robocode/battle/DefaultSpawnController.java
@@ -52,6 +52,7 @@ public final class DefaultSpawnController implements ISpawnController {
 
     @Override
     public double[] getSpawnLocation(RobotPeer r, Battle b) {
+    	Logger.realOut.println("GSL = " + r + ", " + b);
         Random random = RandomFactory.getRandom();
         BattleRules battleRules = b.getBattleRules();
         for (int i = 0; i < controllers.size(); i++) {
