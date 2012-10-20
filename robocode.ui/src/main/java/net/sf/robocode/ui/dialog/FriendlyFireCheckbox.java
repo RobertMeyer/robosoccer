c5bf322ce078519093213a6d01b0f1e252bd43f5
unknown <woairaspberry@hotmail.com>
2012-10-20 22:04:20 +1000

    Added javadocs

--- robocode.ui/src/main/java/net/sf/robocode/ui/dialog/FriendlyFireCheckbox.java
+++ robocode.ui/src/main/java/net/sf/robocode/ui/dialog/FriendlyFireCheckbox.java
@@ -1,3 +1,9 @@
+/**
+ * @author Jonathan W
+ * FriendlyFireCheckBox deals with the 'listening' to the condition of the Friendly Fire Checkbox.
+ * There is a tracker in it to tracker the condition and it will be send to bulletpeer to activate the function.
+ */
+
 package net.sf.robocode.ui.dialog;
 
 import java.awt.event.ActionEvent;
