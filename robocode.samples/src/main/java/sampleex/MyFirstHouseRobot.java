5fbc91cba14c97bc92dbafd43952b3ab8e02f706
Sheng-Yao Wei <neeblestick@gmail.com>
2012-10-20 22:39:20 +1000

    Updated house robot implementation
    house robots start out passive - only shoots if other robots are within
    range
    aggressive - when below 150 energy, shoots at everything
    vendetta - will only shoot at a certain robot when targeted by that
    certain robot (ie. house robot gets hit by the same robot more than 5
    times in a row)

--- robocode.samples/src/main/java/sampleex/MyFirstHouseRobot.java
+++ robocode.samples/src/main/java/sampleex/MyFirstHouseRobot.java
@@ -12,22 +12,36 @@ import robocode.TurnCompleteCondition;
 
 public class MyFirstHouseRobot extends HouseRobot {
 	
+	private final static int PASSIVE = 0;
+	private final static int AGGRESSIVE = 1;
+	private final static int VENDETTA = 2;
+	private int behaviour;
+	private String lastRobot;
+	private int hitCount;
 	HouseRobotBoundary home;
 	
 	public void run() {
 		
-		setColors(Color.RED, Color.RED, Color.RED);
+		setColors(Color.GREEN, Color.GREEN, Color.GREEN);
+		setBulletColor(Color.GREEN);
 		
 		home = getBoundaries();
-		
 		home.setxHome(getX());
 		home.setyHome(getY());
 		home.setInitialFacing(getHeading());
 		home.setArcRange(200);
 		
+		behaviour = PASSIVE;
+		hitCount = 0;
+		
 		this.turnLeft(80);
 		
 		while (true) {
+			if (this.getEnergy() < 150) {
+				behaviour = AGGRESSIVE;
+				setColors(Color.RED, Color.RED, Color.RED);
+				setBulletColor(Color.RED);
+			}
 			if (this.getScannedRobotEvents().size() == 0) {
 				setTurnRight(160);
 				waitFor(new TurnCompleteCondition(this));
@@ -37,24 +51,120 @@ public class MyFirstHouseRobot extends HouseRobot {
 		}
 	}
 	
-	public void onScannedRobot(ScannedRobotEvent e) {		
+	
+	public void onScannedRobot(ScannedRobotEvent e) {	
 		
-		if (e.getDistance() < 200) {
-			
+		switch (behaviour) {
+
+		case '0':
+			if (e.getDistance() < 200) {	
+				setFire(Rules.MAX_BULLET_POWER);
+			}
+			break;
+
+		case '1':
 			setFire(Rules.MAX_BULLET_POWER);
-			turnRight(e.getBearing());
-			setAhead(10);
+			break;
+
+		case '2':
+			if (e.getName() == lastRobot) {
+				setFire(Rules.MAX_BULLET_POWER);
+			}
+			break;
 		}
+
 	}
 	
-	public void onHitRobot(HitRobotEvent e) {		
+	public void onHitRobot(HitRobotEvent e) {
+		
+		lastRobot = e.getName();
+		
+		if (behaviour != AGGRESSIVE) {
 			
-		turnRight(e.getBearing());
-		setFire(Rules.MAX_BULLET_POWER);
+			if (lastRobot == e.getName()) {
+				hitCount += 1;
+			} else {
+				hitCount = 1;
+				behaviour = PASSIVE;
+				setColors(Color.GREEN, Color.GREEN, Color.GREEN);
+				setBulletColor(Color.GREEN);
+			}
+			
+			if (hitCount >= 5) {
+				behaviour = VENDETTA;
+				setColors(Color.BLACK, Color.BLACK, Color.BLACK);
+				setBulletColor(Color.BLACK);
+			}
+			
+		}
+		
+		switch (behaviour) {
+
+		case '0':
+			turnRight(e.getBearing());
+			setFire(Rules.MAX_BULLET_POWER);
+			break;
+
+		case '1':
+			turnRight(e.getBearing());
+			setFire(Rules.MAX_BULLET_POWER);
+			break;
+
+		case '2':
+			if (e.getName() == lastRobot) {
+				turnRight(e.getBearing());
+				setFire(Rules.MAX_BULLET_POWER);
+			}
+			break;
+		}
+			
+		
 	}
 	
 	public void onHitByBullet(HitByBulletEvent e) {
-		turnRight(e.getBearing());
-		setFire(Rules.MAX_BULLET_POWER);
+		
+		lastRobot = e.getName();
+		
+		if (behaviour != AGGRESSIVE) {
+			
+			if (lastRobot == e.getName()) {
+				hitCount += 1;
+			} else {
+				hitCount = 1;
+				behaviour = PASSIVE;
+				setColors(Color.GREEN, Color.GREEN, Color.GREEN);
+				setBulletColor(Color.GREEN);
+			}
+			
+			if (hitCount >= 5) {
+				behaviour = VENDETTA;
+				setColors(Color.BLACK, Color.BLACK, Color.BLACK);
+				setBulletColor(Color.BLACK);
+			}
+			
+		}
+			
+		switch (behaviour) {
+
+		case '0':
+			turnRight(e.getBearing());
+			setFire(Rules.MAX_BULLET_POWER);
+			break;
+
+		case '1':
+			turnRight(e.getBearing());
+			setFire(Rules.MAX_BULLET_POWER);
+			break;
+
+		case '2':
+			if (e.getName() == lastRobot) {
+				turnRight(e.getBearing());
+				setFire(Rules.MAX_BULLET_POWER);
+			}
+			break;
+
+		}
+		
+
 	}
 }
