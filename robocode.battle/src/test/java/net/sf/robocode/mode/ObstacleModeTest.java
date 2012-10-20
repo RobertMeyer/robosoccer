5b78286f8730a08deb56c3ae0e7b85edfb3f09c6
Michael Tsai <michael.tsai@uqconnect.edu.au>
2012-10-21 00:26:54 +1000

    Added some basic ObstacleMode tests

--- robocode.battle/src/test/java/net/sf/robocode/mode/ObstacleModeTest.java
+++ robocode.battle/src/test/java/net/sf/robocode/mode/ObstacleModeTest.java
@@ -2,23 +2,27 @@ package net.sf.robocode.mode;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import net.sf.robocode.battle.Battle;
 import net.sf.robocode.battle.BattleManager;
 import net.sf.robocode.battle.BattleProperties;
 import net.sf.robocode.battle.events.BattleEventDispatcher;
+import net.sf.robocode.battle.peer.ObstaclePeer;
 import net.sf.robocode.host.CpuManager;
 import net.sf.robocode.host.HostManager;
 import net.sf.robocode.host.security.ThreadManager;
 import net.sf.robocode.io.Logger;
 import net.sf.robocode.recording.RecordManager;
 import net.sf.robocode.repository.RepositoryManager;
+import net.sf.robocode.security.HiddenAccess;
 import net.sf.robocode.settings.SettingsManager;
 
 import org.junit.Test;
 import org.junit.Before;
 import org.mockito.Mockito;
 
+import robocode.BattleRules;
 import robocode.control.BattleSpecification;
 import robocode.control.BattlefieldSpecification;
 import robocode.control.RobocodeEngine;
@@ -27,7 +31,7 @@ import robocode.control.RobotSpecification;
 import static org.junit.Assert.*;
 
 public class ObstacleModeTest {
-	/*protected static String robotsPath;
+	protected static String robotsPath;
 	private BattleProperties bp;
 	private BattleManager bm;
 	private HostManager hm;
@@ -38,34 +42,18 @@ public class ObstacleModeTest {
 	private ThreadManager tm;
 	private RecordManager rem;
 	private Battle battle;
-	
-	RobocodeEngine engine;
+	private BattleRules br;
 	
 	@Before
 	public void setup() {
-		System.setProperty("EXPERIMENTAL", "true");
-        System.setProperty("TESTING", "true");
-        System.setProperty("WORKINGDIRECTORY", "target/test-classes");
-        try {
-        	if (new File("").getAbsolutePath().endsWith("robocode.tests")) {
-                robotsPath = new File("../robocode.tests.robots").getCanonicalPath();
-            } else {
-                throw new Error("Unknown directory");
-            }
-        } catch (IOException e) {
-            e.printStackTrace(Logger.realErr);
-        }
-        System.setProperty("ROBOTPATH", robotsPath + "/target/classes");
-        
-        
-		tm = new ThreadManager();
+		/*tm = new ThreadManager();
 		sm = new SettingsManager();
 		hm = new HostManager(sm, tm);
 		rm = new RepositoryManager(sm);
 		cm = new CpuManager(sm);
 		bd = new BattleEventDispatcher();
 		rem = new RecordManager(sm);
-		bm = new BattleManager(sm, rm, hm, cm, bd, rem);
+		bm = new BattleManager(sm, rm, hm, cm, bd, rem);*/
 		
 		bp = Mockito.mock(BattleProperties.class);
 		Mockito.when(bp.getBattlefieldWidth()).thenReturn(800);
@@ -77,29 +65,33 @@ public class ObstacleModeTest {
 		Mockito.when(bp.getSelectedRobots()).thenReturn("sample.Walls, sample.Target");
 		Mockito.when(bp.getInitialPositions()).thenReturn(null);
 		Mockito.when(bp.getBattleMode()).thenReturn(new ObstacleMode());
-
-		//battle = new Battle(sm, bm, hm, rm, cm, bd);
-		//battle.setup(rm.getSelectedRobots(bp.getSelectedRobots()), bp, false, rm);
-		//assert things;
-		RobotSpecification[] rs = engine.getLocalRepository("tested.robots.Ahead, tested.robots.Ahead");
 		
-        assertNotNull("Robot were not loaded", rs);
-        assertEquals("Robot were not loaded", 2, rs.length);
-        engine.runBattle(new BattleSpecification(1, new BattlefieldSpecification(), rs), null, true);
+		battle = Mockito.mock(Battle.class);
+		Mockito.when(battle.getBattleMode()).thenReturn(new ObstacleMode());		
+		
 	}
 	
 	@Test
 	public void testToString() {
-		assertEquals("toString method is incorrect", battle.getBattleMode().toString(), "Obstacle Mode");
+		String output = battle.getBattleMode().toString();
+		assertEquals("toString method is incorrect", output, "Obstacle Mode");
+	}
+	
+	@Test
+	public void testDescription() {
+		String output = battle.getBattleMode().getDescription();
+		assertEquals("Wrong description was provided", output, 
+				"A mode with obstacles that robots have to avoid.");
 	}
 	
 	@Test
 	public void testNumObstacles() {
-		//assertEquals("There are too many obstacles on the battlefield", om.setNumObstacles(null),
-		//		10);
+		List<ObstaclePeer> obstacles = ObstacleMode.generateRandomObstacles(10, bp, br, battle, 32, 32);
+		//battle.setup(rm.getSelectedRobots(bp.getSelectedRobots()), bp, false, rm);
+		assertEquals("Incorrect number of obstacles generated", obstacles.size(), 10);
 	}
 	
-	@Test
+	/*@Test
 	public void testObstacles() {
 		BattleProperties bp = Mockito.mock(BattleProperties.class);
 		// more mocking...
