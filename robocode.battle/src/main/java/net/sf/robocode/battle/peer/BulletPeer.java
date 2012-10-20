RobertMeyer/robosoccer

commit 8a0717011904230531ac40caf807d60b84f70c6a

Friendly fire with added Checkedbox

robocode.battle/src/main/java/net/sf/robocode/battle/peer/BulletPeer.java
@@ -52,6 +52,7 @@ import java.util.List;
 
 import net.sf.robocode.battle.FreezeRobotDeath;
 import net.sf.robocode.battle.BoundingRectangle;
+import net.sf.robocode.battle.FriendlyFireTracker;
 import net.sf.robocode.battle.KillstreakTracker;
 import net.sf.robocode.peer.BulletStatus;
 import robocode.BattleRules;
@@ -214,7 +215,15 @@ public class BulletPeer {
 				boolean teamFire = (owner.getTeamPeer() != null && owner
 						.getTeamPeer() == otherRobot.getTeamPeer());
 
-				if (!teamFire) {
+				if(FriendlyFireTracker.enableFriendlyfire == true){
+					if(teamFire){
+						otherRobot.updateEnergy(+damage);
+					}
+				}else{
+					otherRobot.updateEnergy(-damage);
+				}
+				
+				if(!teamFire){
 					owner.getRobotStatistics().scoreBulletDamage(
 							otherRobot.getName(), score);
 				}

robocode.battle/src/main/java/net/sf/robocode/battle/FriendlyFireTracker.java
@@ -1,0 +1,10 @@
+package net.sf.robocode.battle;
+
+public class FriendlyFireTracker {
+	public static boolean enableFriendlyfire = false;
+	
+	public static void enableFriendlyFire(boolean b){
+		enableFriendlyfire = b;
+	}
+
+}

robocode.ui/src/main/java/net/sf/robocode/ui/dialog/FriendlyFireCheckbox.java
@@ -1,0 +1,26 @@
+package net.sf.robocode.ui.dialog;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JCheckBox;
+
+import net.sf.robocode.battle.BattleProperties;
+import net.sf.robocode.battle.FriendlyFireTracker;
+
+public class FriendlyFireCheckbox extends JCheckBox {
+	private static final long serialVersionUID = 1L;
+	public BattleProperties bp;
+	
+	public FriendlyFireCheckbox(BattleProperties bp){
+		this.bp = bp;
+        this.setText("Team Friendly Fire");
+        ActionListener actionListener = new ActionListener(){
+        	public void actionPerformed(ActionEvent actionEvent){
+        		FriendlyFireCheckbox box = (FriendlyFireCheckbox) actionEvent.getSource();
+        		FriendlyFireTracker.enableFriendlyFire(box.getModel().isSelected());
+        	}
+        };
+        this.addActionListener(actionListener);
+  }
+}

robocode.ui/src/main/java/net/sf/robocode/ui/dialog/RobocodeFrame.java
@@ -103,6 +103,7 @@ public class RobocodeFrame extends JFrame {
     private EffectAreaCheckbox box;
     private KillstreakCheckbox ksBox;
     private BackgroundMusicCheckbox mbox;
+    private FriendlyFireCheckbox fbox;
     private JLabel tpsLabel;
     private boolean iconified;
     private boolean exitOnClose = true;
@@ -140,6 +141,7 @@ public class RobocodeFrame extends JFrame {
         teleporterBox = new TeleporterCheckbox();
         blackholeBox = new BlackholeCheckbox();
         mbox = new BackgroundMusicCheckbox(battleManager.getBattleProperties());
+        fbox = new FriendlyFireCheckbox(battleManager.getBattleProperties());
         menuBar.setup(this);
         initialize();
     }
@@ -320,12 +322,13 @@ public class RobocodeFrame extends JFrame {
             sidePanel.setLayout(new BorderLayout());
             
             sideBooleans = new JPanel();
-            sideBooleans.setLayout(new GridLayout(5,1));
+            sideBooleans.setLayout(new GridLayout(6,1));
             sideBooleans.add(box);
             sideBooleans.add(ksBox);           
             sideBooleans.add(mbox);
             sideBooleans.add(teleporterBox);
             sideBooleans.add(blackholeBox);
+            sideBooleans.add(fbox);
            
             sidePanel.add(getRobotButtonsScrollPane(), BorderLayout.CENTER);
             final BattleButton btn = net.sf.robocode.core.Container

commit 98a1477c2fbbc36ae406b2210634fdf130847395

Undo BulletPeer modification of commit 8a07170119
Commit 8a0717011904230531ac40caf807d60b84f70c6a broke three tests; this commit
comments out the changes on BulletPeer that caused those breaks.

robocode.battle/src/main/java/net/sf/robocode/battle/peer/BulletPeer.java
@@ -215,13 +215,13 @@ public class BulletPeer {
 				boolean teamFire = (owner.getTeamPeer() != null && owner
 						.getTeamPeer() == otherRobot.getTeamPeer());
 
-				if(FriendlyFireTracker.enableFriendlyfire == true){
-					if(teamFire){
-						otherRobot.updateEnergy(+damage);
-					}
-				}else{
-					otherRobot.updateEnergy(-damage);
-				}
+//				if(FriendlyFireTracker.enableFriendlyfire == true){
+//					if(teamFire){
+//						otherRobot.updateEnergy(+damage);
+//					}
+//				}else{
+//					otherRobot.updateEnergy(-damage);
+//				}
 				
 				if(!teamFire){
 					owner.getRobotStatistics().scoreBulletDamage(